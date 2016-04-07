412c8495319f051fc3ea30180671135c1eaccefa
dezieg <adit3a@gmail.com>
2016-04-07 16:46:46 +0700

    Edited Vagrantfile & run_solution.sh

--- solutions/run_solutions.sh
+++ solutions/run_solutions.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
+SLEEP=5
 
+clear
 echo 'Running'
+echo "Getting Vagrant Box ubuntu/trusty64"
+cd solutions
+
+vagrant box add ubuntu/trusty64
+for A in web postgresql cache application
+do
+	echo "=> Preparing $A Server in $SLEEP secs ..."
+	sleep $SLEEP
+	vagrant up $A
+	echo ""
+done
