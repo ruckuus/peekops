71d473b940c9d6eba747665088488f35b83772a8
Utian Ayuba <utianayuba@gmail.com>
2016-04-21 02:05:26 +0700

    Update main.tf

--- solutions/main.tf
+++ solutions/main.tf
@@ -4,139 +4,28 @@ provider "aws" {
   secret_key = "SECRET_KEY"
 }
 
-resource "aws_vpc" "peekops_main" {
-  cidr_block = "10.10.0.0/16"
+resource "aws_vpc" "vpc_peekops" {
+  cidr_block = "192.168.0.0/16"
 
   tags {
-    Name = "peekops_main"
+    Name = "vpc_peekops"
   }
 }
 
-resource "aws_subnet" "peekops_main_nat" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.1.0/24"
+resource "aws_subnet" "subnet_peekops" {
+  vpc_id = "${aws_vpc.vpc_peekops.id}"
+  cidr_block = "192.168.99.0/24"
   map_public_ip_on_launch = "true"
   availability_zone = "ap-southeast-1a"
 
   tags {
-    Name = "peekops_main_app_a"
+    Name = "subnet_peekops"
   }
 }
 
-resource "aws_subnet" "peekops_main_app_a" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.10.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone = "ap-southeast-1a"
+resource "aws_security_group" "sg_peekops_web" {
 
-  tags {
-    Name = "peekops_main_app_a"
-  }
-}
-
-resource "aws_subnet" "peekops_main_app_b" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.11.0/24"
-  map_public_ip_on_launch = "true"
-  availability_zone = "ap-southeast-1b"
-
-  tags {
-    Name = "peekops_main_app_b"
-  }
-}
-
-resource "aws_subnet" "peekops_main_db_a" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.20.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags {
-    Name = "peekops_main_db_a"
-  }
-}
-
-resource "aws_subnet" "peekops_main_db_b" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.21.0/24"
-  availability_zone = "ap-southeast-1b"
-
-  tags {
-    Name = "peekops_main_db_b"
-  }
-}
-
-resource "aws_subnet" "peekops_main_elb_a" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.30.0/24"
-  availability_zone = "ap-southeast-1a"
-
-  tags {
-    Name = "peekops_main_db_a"
-  }
-}
-
-resource "aws_subnet" "peekops_main_elb_b" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-  cidr_block = "10.10.31.0/24"
-  availability_zone = "ap-southeast-1b"
-
-  tags {
-    Name = "peekops_main_db_b"
-  }
-}
-
-resource "aws_internet_gateway" "peekops_igw" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-
-  tags {
-    Name = "peekops_main_igw"
-  }
-}
-
-resource "aws_route_table" "route_main" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.peekops_igw.id}"
-  }
-
-  tags {
-    Name = "peekops_main_route"
-  }
-}
-
-resource "aws_route_table" "route_app" {
-  vpc_id = "${aws_vpc.peekops_main.id}"
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.peekops_igw.id}"
-  }
-
-  tags {
-    Name = "peekops_main_route_app"
-  }
-}
-
-resource "aws_route_table_association" "app_a" {
-  subnet_id = "${aws_subnet.peekops_main_app_a.id}"
-  route_table_id = "${aws_route_table.route_app.id}"
-}
-
-resource "aws_main_route_table_association" "peekops_main_route" {
-    vpc_id = "${aws_vpc.peekops_main.id}"
-    route_table_id = "${aws_route_table.route_main.id}"
-}
-
-resource "aws_route_table_association" "app_b" {
-  subnet_id = "${aws_subnet.peekops_main_app_b.id}"
-  route_table_id = "${aws_route_table.route_app.id}"
-}
-
-resource "aws_security_group" "peekops_bastion" {
-
-  vpc_id = "${aws_vpc.peekops_main.id}"
+  vpc_id = "${aws_vpc.vpc_peekops.id}"
   ingress {
     from_port = 22
     to_port = 22
@@ -144,31 +33,9 @@ resource "aws_security_group" "peekops_bastion" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags {
-    Name = "peekops_main_bastion"
-  }
-}
-
-resource "aws_security_group" "peekops_elb" {
-
-  vpc_id = "${aws_vpc.peekops_main.id}"
   ingress {
     from_port = 80
     to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port = 443
-    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -181,27 +48,25 @@ resource "aws_security_group" "peekops_elb" {
   }
 
   tags {
-    Name = "peekops_main_elb"
+    Name = "sg_peekops_web"
   }
 }
 
-resource "aws_security_group" "peekops_main_app" {
-  name = "peekops_main_app"
-  description = "Security group for app servers."
-  vpc_id = "${aws_vpc.peekops_main.id}"
+resource "aws_security_group" "sg_peekops_app" {
 
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    security_groups = ["${aws_security_group.peekops_bastion.id}", "${aws_security_group.peekops_elb.id}"]
-  }
-
+  vpc_id = "${aws_vpc.vpc_peekops.id}"
   ingress {
     from_port = 22
     to_port = 22
     protocol = "tcp"
-    security_groups = ["${aws_security_group.peekops_bastion.id}"]
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 3000
+    to_port = 3000
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
@@ -212,101 +77,119 @@ resource "aws_security_group" "peekops_main_app" {
   }
 
   tags {
-    Name = "peekops_main_app"
+    Name = "sg_peekops_app"
   }
 }
 
-resource "aws_elb" "peekops_web" {
-  name = "peekops-web"
+resource "aws_security_group" "sg_peekops_cache" {
 
-  subnets = ["${aws_subnet.peekops_main_elb_a.id}","${aws_subnet.peekops_main_elb_b.id}"]
+  vpc_id = "${aws_vpc.vpc_peekops.id}"
+  ingress {
+    from_port = 6379
+    to_port = 6379
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-  listener {
-    instance_port = 80
-    instance_protocol = "http"
-    lb_port = 80
-    lb_protocol = "http"
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "sg_peekops_cache"
   }
 }
 
-resource "aws_launch_configuration" "peekops_app" {
-  name = "peekops-launch-cf"
-  image_id = "ami-eea6678d"
-  instance_type = "t2.micro"
-  key_name = "terraform"
-  security_groups = ["${aws_security_group.peekops_main_app.id}"]
-  associate_public_ip_address = true
+resource "aws_security_group" "sg_peekops_db" {
 
-  lifecycle {
-    create_before_destroy = true
+  vpc_id = "${aws_vpc.vpc_peekops.id}"
+  ingress {
+    from_port = 5432
+    to_port = 5432
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "sg_peekops_db"
   }
 }
 
-resource "aws_autoscaling_group" "peekops_app" {
-  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
-  name = "peekops-asg"
-  max_size = 5
-  min_size = 2
-  health_check_grace_period = 300
-  health_check_type = "ELB"
-  desired_capacity = 3
-  force_delete = true
-  launch_configuration = "${aws_launch_configuration.peekops_app.name}"
-  load_balancers = ["${aws_elb.peekops_web.name}"]
-  vpc_zone_identifier = ["${aws_subnet.peekops_main_app_a.id}","${aws_subnet.peekops_main_app_b.id}"]
-
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  tag {
-    key = "Group"
-    value = "PeekOps Autoscaling Group."
-    propagate_at_launch = true
-  }
+resource "aws_key_pair" "key_terraform" {
+  key_name = "key_terraform" 
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDZoIbsUfcwKXpQVXlCP+6Qf/bCs0WnJiYY8I0iuMaPX+6tMDA9mwRUj6i6gyLhyYa+AVEd1j3MbwJ2oute4Q4Y1QNPZUbC10g9ToYHUp+rCd5x3Jytn8CXztz6Z9KMG+YBy0otjuaeFOFKij0zTuUMZWXYBoKR7q9rKMkedyFMSFRMsjC5kF2oArFJgPAZRBQkWbu9uxYQD6TpGrQnZwPkSbFP1XwX2bZCy0G5XQ+iEbV+y7sS7sL5jitFKg0RKm3yuxKBCT4lyBYdvNVH009Le6IjYtY2xkOhTnADI4nSf5Mu7yU4PIEbAWA4lys84LPoHjZ5ypT3DV6Dle9bgGWZ utianayuba@gmail.com"
 }
 
-resource "aws_instance" "bastion" {
-  connection {
-    user = "ubuntu"
-    key_file = "/path/to/private/key/terraform.pem"
-  }
-
-  ami = "ami-YOURAMIID"
+resource "aws_instance" "inst_peekops_web" {
+  ami = "ami-25c00c46"
   instance_type = "t2.micro"
   availability_zone = ""
   ebs_optimized = "false"
   disable_api_termination = "true"
   instance_initiated_shutdown_behavior = "stop"
-  key_name = "terraform"
-  vpc_security_group_ids = ["${aws_security_group.peekops_bastion.id}"]
-  subnet_id = "${aws_subnet.peekops_main_nat.id}"
+  key_name = "key_terraform"
+  vpc_security_group_ids = ["${aws_security_group.sg_peekops_web.id}"]
+  subnet_id = "${aws_subnet.subnet_peekops.id}"
+  private_ip = "192.168.99.101"
   tags {
-     Name = "peekops_bastion"
+     Name = "inst_peekops_web"
   }
 }
 
-#### Application
-resource "aws_instance" "application" {
-  connection {
-    user = "ubuntu"
-    key_file = "/path/to/private/key/terraform.pem"
-  }
-
-  ami = "ami-YOURAMIID"
+resource "aws_instance" "inst_peekops_app" {
+  ami = "ami-25c00c46"
   instance_type = "t2.micro"
   availability_zone = ""
   ebs_optimized = "false"
   disable_api_termination = "true"
   instance_initiated_shutdown_behavior = "stop"
-  key_name = "terraform"
-  vpc_security_group_ids = ["${aws_security_group.peekops_main_app.id}"]
-  subnet_id = "${aws_subnet.peekops_main_app_a.id}"
+  key_name = "key_terraform"
+  vpc_security_group_ids = ["${aws_security_group.sg_peekops_app.id}"]
+  subnet_id = "${aws_subnet.subnet_peekops.id}"
+  private_ip = "192.168.99.102"
   tags {
-     Name = "peekops_application"
+     Name = "inst_peekops_app"
   }
 }
 
-##### Web
-##### DB (You will need to launch RDS)
-##### Cache (You will need to launch ElastiCache instance)
+resource "aws_instance" "inst_peekops_cache" {
+  ami = "ami-25c00c46"
+  instance_type = "t2.micro"
+  availability_zone = ""
+  ebs_optimized = "false"
+  disable_api_termination = "true"
+  instance_initiated_shutdown_behavior = "stop"
+  key_name = "key_terraform"
+  vpc_security_group_ids = ["${aws_security_group.sg_peekops_cache.id}"]
+  subnet_id = "${aws_subnet.subnet_peekops.id}"
+  private_ip = "192.168.99.103"
+  tags {
+     Name = "inst_peekops_cache"
+  }
+}
+
+resource "aws_instance" "inst_peekops_db" {
+  ami = "ami-25c00c46"
+  instance_type = "t2.micro"
+  availability_zone = ""
+  ebs_optimized = "false"
+  disable_api_termination = "true"
+  instance_initiated_shutdown_behavior = "stop"
+  key_name = "key_terraform"
+  vpc_security_group_ids = ["${aws_security_group.sg_peekops_db.id}"]
+  subnet_id = "${aws_subnet.subnet_peekops.id}"
+  private_ip = "192.168.99.104"
+  tags {
+     Name = "inst_peekops_db"
+  }
+}
